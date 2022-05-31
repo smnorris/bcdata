01e083b8431024905d9b4600644191c43a7010d0
Simon Norris <snorris@hillcrestgeo.ca>
2022-05-31 12:26:31 -0700

    fix #96, sort of

--- bcdata/wfs.py
+++ bcdata/wfs.py
@@ -135,9 +135,14 @@ def get_count(dataset, query=None):
 
 def make_request(parameters):
     """Submit a getfeature request to DataBC WFS and return features"""
-    r = requests.get(bcdata.WFS_URL, params=parameters)
-    log.debug(r.url)
-    return r.json()["features"]
+    try:
+        r = requests.get(bcdata.WFS_URL, params=parameters)
+        log.debug(r.url)
+        r.raise_for_status()                     # check status code is 200
+    except requests.exceptions.HTTPError as err: # fail if not 200
+        raise SystemExit(err)
+    log.debug(r.headers)
+    return r.json()["features"]                  # return features if status code is 200
 
 
 def define_request(
