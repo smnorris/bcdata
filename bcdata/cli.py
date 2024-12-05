4a196c68dd35c4cbe1ae17a45ddf6e9793b4cc4c
Simon Norris <snorris@hillcrestgeo.ca>
2024-12-04 17:17:04 -0800

    bc2parquet proof of concept

--- bcdata/cli.py
+++ bcdata/cli.py
@@ -6,6 +6,12 @@ import sys
 
 import click
 from cligj import compact_opt, indent_opt, quiet_opt, verbose_opt
+from shapely.geometry.linestring import LineString
+from shapely.geometry.multilinestring import MultiLineString
+from shapely.geometry.multipoint import MultiPoint
+from shapely.geometry.multipolygon import MultiPolygon
+from shapely.geometry.point import Point
+from shapely.geometry.polygon import Polygon
 
 import bcdata
 from bcdata.database import Database
@@ -20,6 +26,27 @@ def configure_logging(verbosity):
 
 def complete_dataset_names(ctx, param, incomplete):
     return [k for k in bcdata.list_tables() if k.startswith(incomplete)]
+
+
+def ensure_single_geometry_type(df):
+    """If mix of single/multi part geometries are present, promote all geometries to multipart"""
+    geomtypes = sorted(
+        [t.upper() for t in df.geometry.geom_type.dropna(axis=0, how="all").unique()], key=len
+    )
+    if len(geomtypes) > 1 and geomtypes[1] == "MULTI" + geomtypes[0]:
+        df.geometry = [
+            MultiPoint([feature]) if isinstance(feature, Point) else feature
+            for feature in df.geometry
+        ]
+        df.geometry = [
+            MultiLineString([feature]) if isinstance(feature, LineString) else feature
+            for feature in df.geometry
+        ]
+        df.geometry = [
+            MultiPolygon([feature]) if isinstance(feature, Polygon) else feature
+            for feature in df.geometry
+        ]
+    return df
 
 
 # bounds handling direct from rasterio
@@ -73,9 +100,7 @@ bounds_opt_dem = click.option(
     help='Bounds: "left bottom right top" or "[left, bottom, right, top]". Coordinates are BC Albers (default) or --bounds_crs',
 )
 
-dst_crs_opt = click.option(
-    "--dst-crs", "--dst_crs", default="epsg:4326", help="Destination CRS"
-)
+dst_crs_opt = click.option("--dst-crs", "--dst_crs", default="epsg:4326", help="Destination CRS")
 
 lowercase_opt = click.option(
     "--lowercase", "-l", is_flag=True, help="Write column/properties names as lowercase"
@@ -393,9 +418,7 @@ def bc2pg(
     if refresh and append:
         raise ValueError("Options append and refresh are not compatible")
     if refresh and (schema == "bcdata"):
-        raise ValueError(
-            "Refreshing tables in bcdata schema is not supported, use another schema"
-        )
+        raise ValueError("Refreshing tables in bcdata schema is not supported, use another schema")
     elif refresh and schema:
         schema_target = schema
     elif refresh and not schema:
@@ -406,9 +429,7 @@ def bc2pg(
         if not table:
             table = bcdata.validate_name(dataset).lower().split(".")
         if schema_target + "." + table not in db.tables:
-            raise ValueError(
-                f"Cannot refresh, {schema_target}.{table} not found in database"
-            )
+            raise ValueError(f"Cannot refresh, {schema_target}.{table} not found in database")
     out_table = bcdata.bc2pg(
         dataset,
         db_url,
@@ -439,3 +460,103 @@ def bc2pg(
     # do not notify of data load completion when no data load has occured
     if not schema_only:
         log.info("Load of {} to {} in {} complete".format(dataset, out_table, db_url))
+
+
+@cli.command()
+@click.argument("dataset", type=click.STRING, shell_complete=complete_dataset_names)
+@click.option("--out_file", "-o", help="Output file")
+@click.option(
+    "--query",
+    help="A valid CQL or ECQL query",
+)
+@bounds_opt
+@click.option(
+    "--bounds-crs",
+    "--bounds_crs",
+    help="CRS of provided bounds",
+    default="EPSG:3005",
+)
+@click.option(
+    "--count",
+    "-c",
+    default=None,
+    type=int,
+    help="Total number of features to load",
+)
+@click.option("--sortby", "-s", help="Name of sort field")
+@verbose_opt
+@quiet_opt
+def bc2parquet(
+    dataset,
+    out_file,
+    query,
+    bounds,
+    bounds_crs,
+    count,
+    sortby,
+    verbose,
+    quiet,
+):
+    """Dump table to parquet
+    *Script assumes that all data fits in memory*
+    For larger than memory data, call script the script multiple times (filtering data as required)
+    """
+    # Note that the conversion can easily be done with ogr2ogr (or gpq):
+    #
+    # bcdata dump <dataset> |
+    #   ogr2ogr -f Parquet \
+    #     <dataset>.parquet \
+    #     -lco SORT_BY_BBOX=YES \
+    #     /vsistdin?buffer_limit=-1/
+
+    # this command does much the same, but also cleans the data slightly
+
+    verbosity = verbose - quiet
+    configure_logging(verbosity)
+    log = logging.getLogger(__name__)
+
+    dataset = bcdata.validate_name(dataset)
+
+    # default to writing to <dataset>.parquet
+    if not out_file:
+        out_file = dataset.lower() + ".parquet"
+    
+    # get data
+    gdf = bcdata.get_data(
+        dataset,
+        query=query,
+        bounds=bounds,
+        bounds_crs=bounds_crs,
+        count=count,
+        sortby=sortby,
+        as_gdf=True,
+        crs="epsg:3005",
+        lowercase=True,
+    )
+
+    # make sure geometry is "geometry"
+    if gdf.geometry.name != "geometry":
+        gdf = gdf.rename_geometry("geometry")
+
+    # deal with mixed singlepart/multipart geometry types
+    gdf = ensure_single_geometry_type(gdf)
+
+    # set other data types by introspecting the data
+    gdf = gdf.convert_dtypes()
+
+    # preserve column order, remove redundant/unsuported columns
+    table_definition = bcdata.get_table_definition(dataset)
+    column_names = [
+        c["column_name"].lower()
+        for c in table_definition["schema"]
+        if c["column_name"] not in ["FEATURE_AREA_SQM", "FEATURE_LENGTH_M"]
+        and c["data_type"] in ["NUMBER", "VARCHAR2", "DATE"]
+    ]
+    gdf = gdf[column_names]
+
+    log.info(f"Writing {dataset} to parquet: {out_file}")
+
+    # we don't bother sorting by bbox because:
+    # - geopandas doesn't currently offer the option
+    # - for existing tools, we want all data anyway
+    gdf.to_parquet(out_file)
